42048c6b6e6ccd457d27469e06881910b1886c3a
Kamron Safarnejad <kamronsafarnejad@Kamrons-MacBook-Air-2.local>
2025-08-28 16:54:49 -0700

    promo code option added?

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -28,6 +28,7 @@ export default async function handler(req, res) {
       metadata: {
         firebaseUid: uid,
       },
+      allow_promotion_codes: true
     });
 
     return res.status(200).json({ url: session.url });
